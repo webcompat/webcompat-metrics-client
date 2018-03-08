c413e6f1d451bab6a0cb2082f78229cae5db5d36
Mike Taylor <miket@mozilla.com>
2018-03-08 17:15:08 -0600

    Issue #9. Show that we're getting API data.

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello</div>, document.getElementById("appMountPoint"));
+class ProxiedAPI extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { results: [] };
+  }
+
+  componentDidMount() {
+    fetch("/api/needsdiagnosis").then(res =>
+      res.json().then(data => {
+        console.log(data);
+        this.setState({ results: data });
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Open the console</h1>
+        we got this many things: {this.state.results.length}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ProxiedAPI />, document.getElementById("appMountPoint"));
