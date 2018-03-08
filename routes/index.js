7c26934693152d0eeed6bdbaf3c4d2dc95e78590
magsout <guillaume.demesy@gmail.com>
2018-03-08 21:55:59 +0100

    Added css linter, scripts and eslint react

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
--- routes/needsdiagnosis.js
+++ routes/needsdiagnosis.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
 
 /* GET needsdiagnosis dashboard and related routes. */
 router.get("/", function(req, res, next) {
