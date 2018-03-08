1145e2a512d333d359ad2c3296aecf09c714e6f6
Mike Taylor <miket@mozilla.com>
2018-03-08 17:20:33 -0600

    Issue #9. Make eslint happy.

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+const path = require("path");
 
-var index = require("./routes/index");
-var needsdiagnosis = require("./routes/needsdiagnosis");
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var app = express();
+const index = require("./routes/index");
+const needsdiagnosis = require("./routes/needsdiagnosis");
+
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +25,7 @@ app.use("/needsdiagnosis", needsdiagnosis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
