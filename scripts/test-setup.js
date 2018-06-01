e22c800a9589f9975def95e1caa2f57579c2a829
magsout <guillaume.demesy@gmail.com>
2018-06-01 15:46:29 +0200

    Added fetch mock

--- scripts/test-setup.js
+++ scripts/test-setup.js
@@ -1,5 +1,7 @@
 // setup file
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+global.fetch = require("jest-fetch-mock");
 
 configure({ adapter: new Adapter() });
