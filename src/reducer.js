fa277a0ebc302e881eb8599eb3da51174eb500ee
laghee <katrua@gmail.com>
2018-11-06 17:26:57 +0100

    Issue #107 - Fix linting error in order to expose rest of errors

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,6 @@ import { combineReducers } from "redux";
 import needsDiagnosis, {
   STATE_KEY as NEEDS_DIAGNOSIS_STATE_KEY,
 } from "./modules/needsDiagnosis";
-
 import weeklyReports, {
   STATE_KEY as WEEKLY_REPORTS_STATE_KEY,
 } from "./modules/weeklyReports";
