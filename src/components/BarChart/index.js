af6cd80a7077975433b96971ad9c7088904735f7
magsout <magsout@gmail.com>
2019-01-02 18:47:05 +0100

    Issue #131 - display a label of the weekly value

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bar } from "react-chartjs-2";
+import { Bar, Chart } from "react-chartjs-2";
 
 import {
   propTypes,
@@ -8,16 +8,63 @@ import {
 } from "../../propTypes/ChartPropTypes.js";
 import { Container } from "../Chart";
 
-const BarChart = props => {
-  const data = {
-    ...hydrateData(props),
-  };
-  return (
-    <Container title={props.title} subtitle={props.subtitle}>
-      <Bar data={data} options={props.options} legend={props.legend} />
-    </Container>
-  );
-};
+class BarChart extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    Chart.pluginService.register({
+      afterDraw: (chart, easing) => {
+        const ctx = chart.ctx;
+        chart.data.datasets.forEach((dataset, i) => {
+          const meta = chart.getDatasetMeta(i);
+          if (!meta.hidden) {
+            meta.data.forEach((element, index) => {
+              // Draw the text in black, with the specified font
+              ctx.fillStyle = "rgb(0, 0, 0)";
+              const fontSize = 16;
+              const fontStyle = "normal";
+              const fontFamily = "Helvetica Neue";
+              ctx.font = Chart.helpers.fontString(
+                fontSize,
+                fontStyle,
+                fontFamily,
+              );
+              // Just naively convert to string for now
+              const dataString = dataset.data[index].toString();
+              // Make sure alignment settings are correct
+              ctx.textAlign = "center";
+              ctx.textBaseline = "middle";
+              const padding = 5;
+              const position = element.tooltipPosition();
+              ctx.fillText(
+                dataString,
+                position.x,
+                position.y - fontSize / 2 - padding,
+              );
+            });
+          }
+        });
+      },
+    });
+  }
+
+  render() {
+    const data = {
+      ...hydrateData(this.props),
+    };
+    return (
+      <Container title={this.props.title} subtitle={this.props.subtitle}>
+        <Bar
+          data={data}
+          options={this.props.options}
+          legend={this.props.legend}
+        />
+      </Container>
+    );
+  }
+}
 
 BarChart.propTypes = {
   ...propTypes,
