aabccc6a1ca994c778ea4cb6df85d24c6f69472e
magsout <magsout@gmail.com>
2020-04-10 14:13:41 +0200

    Issue #271 -  Favicons
    - Added favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,6 +17,26 @@ class Document extends NextDocument {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
